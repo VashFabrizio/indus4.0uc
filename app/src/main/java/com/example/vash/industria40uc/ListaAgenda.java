6632697024e9a5656580a8b4be30bb2f8e4b97c2
Gino <gino_gjtm@hotmail.com>
2019-11-06 09:02:03 -0500

    Letra minuscula agenda

--- app/src/main/java/com/example/vash/industria40uc/ListaAgenda.java
+++ app/src/main/java/com/example/vash/industria40uc/ListaAgenda.java
@@ -1,12 +1,12 @@
 package com.example.vash.industria40uc;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class ListaAgenda extends AppCompatActivity {
 
